91dce6e13a85ffe5886c730539d5baf310dd2a86
yinyangStudio99 <yinyang_studio@hotmail.com>
2023-01-17 18:12:42 +0100

    Upload

---
+++ data.js
@@ -0,0 +1,155 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bad_panorama_k01",
+      "name": "BAD_Panorama_K01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04406006507255,
+          "pitch": 0.007865151221295719,
+          "rotation": 0,
+          "target": "1-bad_panorama_k02"
+        },
+        {
+          "yaw": 1.004979293360206,
+          "pitch": 0.010973461769058446,
+          "rotation": 0,
+          "target": "2-bad_panorama_k03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bad_panorama_k02",
+      "name": "BAD_Panorama_K02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.704658110717272,
+          "pitch": 0.01679249451742848,
+          "rotation": 0,
+          "target": "2-bad_panorama_k03"
+        },
+        {
+          "yaw": 1.6321022429931444,
+          "pitch": 0.004196906634264863,
+          "rotation": 0,
+          "target": "0-bad_panorama_k01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bad_panorama_k03",
+      "name": "BAD_Panorama_K03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9184205437244763,
+          "pitch": -0.005941871879230121,
+          "rotation": 0,
+          "target": "1-bad_panorama_k02"
+        },
+        {
+          "yaw": -2.260785543740596,
+          "pitch": -0.007471991511438958,
+          "rotation": 0,
+          "target": "0-bad_panorama_k01"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
